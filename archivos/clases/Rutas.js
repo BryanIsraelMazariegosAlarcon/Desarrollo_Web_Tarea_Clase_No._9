dfca8f2ee474a4a22714db1662421cf172dcbf26
BryanIsraelMazariegosAlarcón <bryanisrael76@yahoo.com>
2021-10-01 00:33:01 -0600

    Actualización No. 5

--- archivos/clases/Rutas.js
+++ archivos/clases/Rutas.js
@@ -1,10 +1,5 @@
 var funcion = require('./funcion');
 
-
-// Ruta para tamaño
-//es la funcion de request y respond para poder ordenarlo por tamano
-//en el res le mandamos el archivo html para que nos lleve al archivo cuando le damos click al boton enviar 
-//y tambien se le pasa el arreglo que se obtiene con las funciones que seria funcion.tamanio
 exports.buscar_porTamanio = function (req, res) {
 
     const arreglo = funcion.tamanio();
@@ -13,9 +8,6 @@ exports.buscar_porTamanio = function (req, res) {
     });
 }
 
-// Ruta experiencia
-//lo mismo de arriba pero aqui y tambien se le pasa el arreglo que se obtiene con las funciones que seria puntosbase
-//que esta recibiendo lo minimo y lo maximo
 exports.Buscar_porExperiencia = function (req, res) {
     const puntosmin = req.body.min_base;
     const puntosmax = req.body.max_base;
@@ -25,10 +17,6 @@ exports.Buscar_porExperiencia = function (req, res) {
     });
 }
 
-// Ruta informacion
-//aqui se recibe como parametro el nombre del pokemon y verifica si obtiene sus datos y si existe se envia el nombre
-//y la imagen del pokemon
-//y si no aparece que no existe
 exports.buscar_porNombre = function (req, res) {
     const nombre = req.body.nombre;
     const arreglo = funcion.buscar_nombre(nombre);
@@ -44,8 +32,6 @@ exports.buscar_porNombre = function (req, res) {
     }
 }
 
-// Ruta para tipos
-//este lo usamos para cargar los tipos de todos los pokemons
 exports.enlistar_tipos = function (req, res) {
     const arreglo = funcion.enlistartipos();
     res.render('index.html', {
@@ -53,7 +39,6 @@ exports.enlistar_tipos = function (req, res) {
     });
 }
 
-//y aqui recibe el tipo y lo busca y nos devuelve un arreglo
 exports.buscar_porTipo = function (req, res) {
     const tipo = req.body.t_pokemon;
     const arreglo = funcion.buscar_tipos(tipo);
